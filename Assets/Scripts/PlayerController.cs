3768b8932edff5eb468b46da66bd2d948b8f1d4f
Jeremiah Franczyk <thefranczyk@gmail.com>
2020-12-17 21:40:17 -0500

    Initial Commit, basic game stucture, fonts, art assets

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    Rigidbody rb;
+    [SerializeField] int rocketJumpHeight = 10;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+
+    private void FixedUpdate()
+    {
+        PlayerMovement();
+    }
+
+
+
+
+    private void PlayerMovement()
+    {
+        if (Input.GetKey("d") || Input.GetKey("right"))
+        {
+            rb.velocity = new Vector3(2, rb.velocity.y, 0);
+        }
+        else if (Input.GetKey("a") || Input.GetKey("left"))
+        {
+            rb.velocity = new Vector3(-2, rb.velocity.y, 0);
+        }
+        //Regular Jump
+        else if (Input.GetKey("space"))
+        {
+            rb.velocity = new Vector3(rb.velocity.x, 1, 0);
+        }
+        //Rocket Jump
+        else if (Input.GetMouseButtonDown(0))
+        {
+            rb.velocity = new Vector3(rb.velocity.x, rocketJumpHeight, 0);
+        }
+    }
+}
---
+++ Assets/Scripts/WinDoor.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class WinDoor : MonoBehaviour
+{
+
+    public void LoadNextScene()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex + 1);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        LoadNextScene();
+    }
+}
