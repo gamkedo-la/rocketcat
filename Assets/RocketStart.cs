6ba0027c9b5ef31eb99707e8d7707ce73baa1353
Jeremiah Franczyk <thefranczyk@gmail.com>
2020-12-23 22:38:28 -0500

    Added Rocket Tracking Code

---
+++ Assets/RocketStart.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RocketStart : MonoBehaviour
+{
+    public Transform testTarget;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //transform.LookAt(testTarget);
+        float ang = Mathf.Atan2(testTarget.position.y - transform.position.y, testTarget.position.x - transform.position.x);
+        transform.rotation = Quaternion.Euler(0.0f, 0.0f, ang * Mathf.Rad2Deg);
+    }
+}
