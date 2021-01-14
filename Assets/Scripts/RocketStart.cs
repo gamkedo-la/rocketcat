23deea23e3bd1f9eaaf53d0f1de99c2b66ee0771
Jeremiah Franczyk <thefranczyk@gmail.com>
2021-01-14 00:57:25 -0500

    Added basic rocket sound and rocket sound code

--- Assets/Scripts/RocketStart.cs
+++ Assets/Scripts/RocketStart.cs
@@ -8,6 +8,10 @@ public class RocketStart : MonoBehaviour
     public GameObject rocketPrefab;
     public Transform rocketFrom;
 
+    [Header ("Sound")]
+    [SerializeField] AudioClip rocketExplodeSound;
+    [SerializeField] [Range (0,1)] public float soundVolume = 0.7f; 
+
     // Start is called before the first frame update
     void Start()
     {
@@ -24,6 +28,7 @@ public class RocketStart : MonoBehaviour
         {
             GameObject.Instantiate(rocketPrefab, rocketFrom.position, rocketFrom.rotation, null);
             Debug.Log("Rocket Launch");
+            AudioSource.PlayClipAtPoint(rocketExplodeSound, Camera.main.transform.position, soundVolume);
         }
         
 
