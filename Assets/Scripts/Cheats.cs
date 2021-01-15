c9a23f6f668039b14c851706a32c1b33c1d032ba
Sifika <50903597+Sifika@users.noreply.github.com>
2021-01-15 09:57:51 +0100

    Quick and dirty cheatcodes
    R = reload level
    O = next level
    I = previous level

---
+++ Assets/Scripts/Cheats.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Cheats : MonoBehaviour
+{
+
+ void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name, LoadSceneMode.Single);
+        }
+
+         if (Input.GetKeyDown(KeyCode.O))
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+            }
+         if (Input.GetKeyDown(KeyCode.I))
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+            }
+    }
+}
