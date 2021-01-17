b09cf2a57f70790766ed231302ae7034ed9d9d5f
Jeremiah Franczyk <thefranczyk@gmail.com>
2021-01-16 19:41:08 -0500

    Added Alien Rocket Launcher to all odd numbered levels and regular rocket launcher to all even levels; added RocketCatModel without lights & camera

---
+++ Assets/Scripts/WeaponSwitch.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponSwitch : MonoBehaviour
+{
+
+    private void Start()
+    {
+        GameObject.Find("Rocket Launcher").SetActive(true);
+        GameObject.Find("Rocket Launcher Alien").SetActive(false); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown("1"))
+        {
+            GameObject.Find("Rocket Launcher").SetActive(true);
+            GameObject.Find("Rocket Launcher Alien").SetActive(false);
+        }
+
+        if (Input.GetKeyDown("2"))
+        {
+            GameObject.Find("Rocket Launcher Alien").SetActive(true);
+            GameObject.Find("Rocket Launcher").SetActive(false);
+            
+        }
+
+
+    }
+}
