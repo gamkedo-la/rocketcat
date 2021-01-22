9eea437640f6709cb553f620082f2973436b4b8b
Jeremiah Franczyk <thefranczyk@gmail.com>
2021-01-22 18:01:20 -0500

    Added Restart Text script that makes "Press R to Restart" text appear when the player is out of rockets

---
+++ Assets/Scripts/RestartText.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RestartText : MonoBehaviour
+{
+    private RocketCountUpdate rcu;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rcu = GameObject.FindGameObjectWithTag("RocketsLeft").GetComponent<RocketCountUpdate>();
+        gameObject.GetComponent<Text>().enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (rcu.rocketCount <= rcu.rocketLimit)
+        {
+            gameObject.GetComponent<Text>().enabled = true;
+        }
+
+        else
+            gameObject.GetComponent<Text>().enabled = false;
+    }
+}
