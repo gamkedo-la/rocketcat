72c88adf4ae9f142281e7a97cd5da74bf67c0545
Tyler Funk <tcfunk25@gmail.com>
2021-01-22 22:37:58 -0600

    Add grunt animations, anim controller, script

---
+++ Assets/MechPatrol.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MechPatrol : MonoBehaviour
+{
+    public Transform[] waypoints;
+    public Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var waypointReached = true;
+
+        if (waypoints.Length == 0 || waypointReached)
+        {
+            animator.SetTrigger("WaypointReached");
+        }
+    }
+}
