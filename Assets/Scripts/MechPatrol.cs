e593dcda4125c5e829f6fae698e8b059fc7ea0ab
Jeremiah Franczyk <thefranczyk@gmail.com>
2021-01-27 10:55:33 -0500

    started mech patrol script- not finished yet

--- Assets/Scripts/MechPatrol.cs
+++ Assets/Scripts/MechPatrol.cs
@@ -7,19 +7,56 @@ public class MechPatrol : MonoBehaviour
     public Transform[] waypoints;
     public Animator animator;
 
-    // Start is called before the first frame update
+    public float speed;
+    private float waitTime;
+    public float startWaitTime;
+
+    public Transform[] moveSpots;
+    private int randomSpot;
+
     void Start()
     {
+        waitTime = startWaitTime;
+        randomSpot = Random.Range(0, moveSpots.Length);
     }
 
     // Update is called once per frame
     void Update()
     {
-        var waypointReached = true;
+        transform.position = Vector2.MoveTowards(transform.position, moveSpots[randomSpot].position, speed * Time.deltaTime);
 
+        if(Vector2.Distance(transform.position, moveSpots[randomSpot].position) < 0.2f)
+        {
+            if(waitTime <= 0)
+            {
+                randomSpot = Random.Range(0, moveSpots.Length);
+            }
+            else
+            {
+                waitTime -= Time.deltaTime;
+            }
+        }
+
+        var waypointReached = true;
         if (waypoints.Length == 0 || waypointReached)
         {
             animator.SetTrigger("WaypointReached");
         }
     }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if(other.gameObject.CompareTag("Obstacle"))
+        {
+            gameObject.transform.Rotate(0, 180, 0);
+        }
+        else
+        {
+            return;
+        }
+    }
+
+
+
+
 }
