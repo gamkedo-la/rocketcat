38e822775c59307e071c08fe13777fdc54838bcc
Jeremiah Franczyk <thefranczyk@gmail.com>
2021-01-27 22:30:55 -0500

    Enemies when firing now emit one of 3 laser fire sounds

--- Assets/Scripts/EnemyGrunt.cs
+++ Assets/Scripts/EnemyGrunt.cs
@@ -4,18 +4,26 @@ using UnityEngine;
 
 public class EnemyGrunt : MonoBehaviour
 {
-
+    [Header("Fire Projectile")]
     public Transform player;
     public GameObject projectile;
     public Transform laserFrom;
     [SerializeField] GameObject laserSpawnPoint;
-
     [SerializeField] float minTimeBetweenShots = 2f;
     [SerializeField] float maxTimeBetweenShots = 3.5f;
-
     private float timeBetweenShots;
     public float startTimeBetweenShots;
 
+    [Header("Sounds")]
+    public AudioSource audioSource;
+    public AudioClip[] audioClipArray;
+    [SerializeField] [Range(0, 1)] float fireSoundVol = 1f;
+
+
+    private void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
 
 
     // Start is called before the first frame update
@@ -29,18 +37,22 @@ public class EnemyGrunt : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Fire();
+    }
+
+
+    public void Fire()
+    {
         if (timeBetweenShots <= 0)
         {
             Instantiate(projectile, laserFrom.position, laserFrom.rotation);
             timeBetweenShots = Random.Range(minTimeBetweenShots, maxTimeBetweenShots);
+            audioSource.clip = audioClipArray[Random.Range(0, audioClipArray.Length)];
+            audioSource.PlayOneShot(audioSource.clip, fireSoundVol);
         }
         else
         {
-            timeBetweenShots -= Time.deltaTime; 
+            timeBetweenShots -= Time.deltaTime;
         }
     }
-
-
-
-
 }
