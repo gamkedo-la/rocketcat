89f16ca7cba595d4f3f1d160ee529d3ec63f37f2
Jeremiah Franczyk <thefranczyk@gmail.com>
2021-02-03 00:08:10 -0500

    Added UI Sounds to start screen

---
+++ Assets/Scripts/StartScreenSounds.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartScreenSounds : MonoBehaviour
+{
+
+    public AudioSource onClickSound;
+    public AudioSource mouseSound;
+    public AudioClip mouseOverSound;
+
+
+
+    public void playSoundEffect()
+    {
+        onClickSound.Play();
+    }
+
+    public void OnMouseOver()
+    {
+        Debug.Log("mouseOverSound Sound Played");
+        mouseSound.PlayOneShot(mouseOverSound, 1f);
+    }
+
+
+}
