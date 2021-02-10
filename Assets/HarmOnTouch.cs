bf2bce7a6991eb7c4219df75f1b2365ee4752b0f
Jeremiah Franczyk <thefranczyk@gmail.com>
2021-02-09 21:52:01 -0500

    DangerElement added: takes health off in time based increments

---
+++ Assets/HarmOnTouch.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HarmOnTouch : MonoBehaviour
+{
+    Health toHarm;
+    float timeTilNextHarm = 0.0f;
+    float hurtFreq = 0.25f;
+
+    private void Update()
+    {
+        if(toHarm)
+        {
+            timeTilNextHarm -= Time.deltaTime;
+            if(timeTilNextHarm <= 0.0f)
+            {
+                toHarm.DealDamage();
+                timeTilNextHarm = hurtFreq;
+            }
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if(other.gameObject.CompareTag("Player"))
+        {
+            toHarm = other.collider.GetComponent<Health>();
+            toHarm.DealDamage();
+            timeTilNextHarm = hurtFreq;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            toHarm = null;
+        }
+    }
+
+}
