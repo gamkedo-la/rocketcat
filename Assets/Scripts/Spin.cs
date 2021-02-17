ec42d051f556bc7bfd950e8b92a0d0bfb87e967b
Christer Kaitila <breakdance@mcfunkypants.com>
2021-02-17 14:59:40 -0800

    fractal (Sierpinski's Triangle / Pascal's Triangle) crystal collectible of joy from another dimension

---
+++ Assets/Scripts/Spin.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spin : MonoBehaviour {
+
+    public Vector3 spinSpeeds;
+
+	void Update () {
+        transform.Rotate(Vector3.up, Time.deltaTime * spinSpeeds.y);		
+        transform.Rotate(Vector3.left, Time.deltaTime * spinSpeeds.x);		
+        transform.Rotate(Vector3.forward, Time.deltaTime * spinSpeeds.z);		
+	}
+}
