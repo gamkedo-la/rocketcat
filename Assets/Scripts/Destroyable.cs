108a4d5736696ba486744c51f56c1140e74bda38
Jeremiah Franczyk <thefranczyk@gmail.com>
2021-02-16 21:54:46 -0500

    Shield flipping now works as well as deflect

--- Assets/Scripts/Destroyable.cs
+++ Assets/Scripts/Destroyable.cs
@@ -4,9 +4,15 @@ using UnityEngine;
 
 public class Destroyable : MonoBehaviour
 {
+    public bool destroyParentAlso = false;
+
   public void DestroySelf()
     {
         Destroy(gameObject);
+        if (destroyParentAlso)
+        {
+            Destroy(transform.parent.gameObject);
+        }
         EnemyCountUpdate.instance.RemoveFromCounterNextFrame();
     }
     
--- Assets/Scripts/EnemyShieldmaiden.cs
+++ Assets/Scripts/EnemyShieldmaiden.cs
@@ -4,17 +4,27 @@ using UnityEngine;
 
 public class EnemyShieldmaiden : MonoBehaviour
 {
-
+    public Transform shield;
+    private Vector3 relativeShieldPos;
+    private bool flipped = false;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        relativeShieldPos = shield.position - transform.position;
+        StartCoroutine(ShieldFlip());
     }
 
-    // Update is called once per frame
-    void Update()
+    
+    IEnumerator ShieldFlip()
     {
-        
+        while(true)
+        {
+            yield return new WaitForSeconds(2.0f);
+            flipped = !flipped;
+            shield.position = transform.position + relativeShieldPos * (flipped ? -1.0f : 1.0f);
+        }
     }
+
+
 }
