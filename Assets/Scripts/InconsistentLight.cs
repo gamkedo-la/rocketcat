f1b0fd1e228aa3aee2ff0330d9f0322c2e901255
Jeremiah Franczyk <thefranczyk@gmail.com>
2021-02-23 21:31:16 -0500

    Added lighting solution for Flicker level

---
+++ Assets/Scripts/InconsistentLight.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InconsistentLight : MonoBehaviour
+{
+
+    Light myLight;
+    public float darkIntensity = 0.1f;
+    public float lightIntensity = 1.2f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myLight = gameObject.GetComponent<Light>();
+        StartCoroutine(LightChange());
+    }
+
+
+
+    IEnumerator LightChange()
+    {
+        while(true)
+        {
+            myLight.intensity = darkIntensity;
+            yield return new WaitForSeconds(Random.Range(3f, 5f));
+            myLight.intensity = lightIntensity;
+            yield return new WaitForSeconds(Random.Range(0.1f, 1.5f));
+        }
+    }
+
+
+}
