1126b415aafdd109db64100fa4e452e18a1d03f4
Jeremiah Franczyk <thefranczyk@gmail.com>
2021-02-23 21:48:49 -0500

    Spotlight initial version working (WIP)

---
+++ Assets/Scripts/OscillatingLight.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OscillatingLight : MonoBehaviour
+{
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.rotation = Quaternion.AngleAxis(45.0f * Mathf.Cos(Time.timeSinceLevelLoad * 0.5f), Vector3.forward) * 
+            Quaternion.AngleAxis(90.0f, Vector3.right); 
+    }
+}
