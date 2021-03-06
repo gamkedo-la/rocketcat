38fc7fd50195bc86c1f1bdfc805e51a8d5946d17
Jeremiah Franczyk <thefranczyk@gmail.com>
2021-03-05 21:47:09 -0500

    Changed Build settings swapping levels 8 and 10; added music for early levels and main menu

---
+++ Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+   
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void LoadNextLevel()
+    {
+
+    }
+
+
+
+}
---
+++ Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour
+{
+    private static MusicPlayer instance;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(instance);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+
+
+}
