667f6245322207c83df0d2588479c6a65d7e09f2
Jeremiah Franczyk <thefranczyk@gmail.com>
2021-03-09 21:56:53 -0500

    boss spawns debris, debris is harmful to player

---
+++ Assets/Scripts/SelfDestruct.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelfDestruct : MonoBehaviour
+{
+    public float timeToDestroy = 3.0f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Destroy(gameObject, timeToDestroy);
+    }
+
+  
+}
---
+++ Assets/Scripts/SpawnWithForce.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnWithForce : MonoBehaviour
+{
+    public Transform containedSpawned;
+    public GameObject prefabToSpawn;
+    public float minSpawnDelay = .2f;
+    public float maxSpawnDelay = 1.0f;
+    public float minForce = 100f;
+    public float maxForce = 600f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(ShootRepeatedly());
+    }
+
+    // Update is called once per frame
+    IEnumerator ShootRepeatedly()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(Random.Range(minSpawnDelay, maxSpawnDelay));
+            GameObject newGO = GameObject.Instantiate(prefabToSpawn);
+            if(containedSpawned != null)
+            {
+                newGO.transform.SetParent(containedSpawned);
+            }
+            else
+            {
+                Debug.Log("Please set contained spawn");
+            }
+            Rigidbody2D rb = newGO.GetComponent<Rigidbody2D>();
+            rb.AddForce(transform.up * Random.Range(minForce, maxForce));
+        }
+    }
+
+
+
+
+}
