8885e889da24fa4e480986efa3e129e614e290fb
Jeremiah Franczyk <thefranczyk@gmail.com>
2021-03-19 22:14:37 -0400

    Boss now instantiates rocket reload crates on a random.range time value

--- Assets/Scripts/SpawnWithForce.cs
+++ Assets/Scripts/SpawnWithForce.cs
@@ -10,11 +10,15 @@ public class SpawnWithForce : MonoBehaviour
     public float maxSpawnDelay = 1.0f;
     public float minForce = 100f;
     public float maxForce = 600f;
+    public float minReloadSpawnDelay = 3.0f;
+    public float maxReloadSpawnDelay = 7.0f;
+    public GameObject reloadCrateToSpawn;
 
     // Start is called before the first frame update
     void Start()
     {
         StartCoroutine(ShootRepeatedly());
+        StartCoroutine(ShootReloadCrateRepeatedly());
     }
 
     // Update is called once per frame
@@ -37,7 +41,24 @@ public class SpawnWithForce : MonoBehaviour
         }
     }
 
-
+    IEnumerator ShootReloadCrateRepeatedly()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(Random.Range(minReloadSpawnDelay, maxReloadSpawnDelay));
+            GameObject newGO = GameObject.Instantiate(reloadCrateToSpawn);
+            if (containedSpawned != null)
+            {
+                newGO.transform.SetParent(containedSpawned);
+            }
+            else
+            {
+                Debug.Log("Please set contained spawn");
+            }
+            Rigidbody2D rb = newGO.GetComponent<Rigidbody2D>();
+            rb.AddForce(transform.up * Random.Range(minForce, maxForce));
+        }
+    }
 
 
 }
