c18dc5f0d3f0b5a178eff33ee5e285fb4b839b68
Jeremiah Franczyk <thefranczyk@gmail.com>
2021-03-19 21:23:58 -0400

    Added Level Name text and fade in fade out animation

---
+++ Assets/Scripts/LevelText.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelText : MonoBehaviour
+{
+    public Animator levelText;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(WaitForTime());
+    }
+
+    IEnumerator WaitForTime()
+    {
+        yield return new WaitForSeconds(4.0f);
+        levelText.SetTrigger("Start");
+    }
+
+}
