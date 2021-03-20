a8e8a65c4ea19f6bd50600c834c0d14a8ccf8957
Jeremiah Franczyk <thefranczyk@gmail.com>
2021-03-19 23:39:11 -0400

    Added coin of joy UI placement (need 2D sprite) and created and hooked up coin of joy code

---
+++ Assets/FractalCoinCountUpdate.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FractalCoinCountUpdate : MonoBehaviour
+{
+    public static FractalCoinCountUpdate instance;
+
+    public static int enteredLevelWith = 0;
+    public static int fractalCoinCount;
+    Text displayText;
+
+
+    private void Awake()
+    {
+        fractalCoinCount = enteredLevelWith;
+        instance = this;
+    }
+
+    private void Start()
+    {
+        displayText = gameObject.GetComponent<Text>();
+        displayText.text = fractalCoinCount.ToString();
+    }
+
+
+    public void UpdateCoinFractalCount()
+    {
+        fractalCoinCount++;
+        displayText.text = fractalCoinCount.ToString();
+    }
+
+
+}
---
+++ Assets/Scripts/FractalCoinCollectible.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FractalCoinCollectible : MonoBehaviour
+{
+    [Header("Sound")]
+    [SerializeField] AudioClip fractalCollectibleSound;
+    [SerializeField] [Range(0, 1)] public float soundVolume = 0.7f;
+
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            FractalCoinCountUpdate.instance.UpdateCoinFractalCount();
+            AudioSource.PlayClipAtPoint(fractalCollectibleSound, Camera.main.transform.position, soundVolume);
+            GameManager.instance.PlaySound();
+            Destroy(gameObject);
+        }
+    }
+}
