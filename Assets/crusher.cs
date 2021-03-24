96c5373e936d01ec2c2e18061cb3ceb270b81bb5
Jeremiah Franczyk <thefranczyk@gmail.com>
2021-03-23 22:37:02 -0400

    Added crusher prefab and code into the game into the game

---
+++ Assets/crusher.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class crusher : MonoBehaviour
+{
+    public Transform start;
+    public Transform end;
+    public Transform smasher;
+    public float rate = 1.0f;
+    public float period = 1.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float posPerc = 0.0f;
+        posPerc = (Mathf.Cos(Time.timeSinceLevelLoad/period) * 0.5f + 0.5f) * rate;
+        posPerc = Mathf.Clamp01(posPerc);
+        smasher.transform.position = posPerc * start.transform.position + (1.0f - posPerc) * end.transform.position;
+        smasher.transform.rotation = Quaternion.Slerp(start.transform.rotation, end.transform.rotation, posPerc);
+    }
+
+}
