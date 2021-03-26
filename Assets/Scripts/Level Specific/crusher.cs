338ea84cf17d2a95a1a800d9d54b9436f6a96c57
Jeremiah Franczyk <thefranczyk@gmail.com>
2021-03-25 23:06:21 -0400

    The Space Mill level complete

--- Assets/Scripts/Level Specific/crusher.cs
+++ Assets/Scripts/Level Specific/crusher.cs
@@ -7,19 +7,26 @@ public class crusher : MonoBehaviour
     public Transform start;
     public Transform end;
     public Transform smasher;
+    public float crushPhaseShift = 0.0f;
     public float rate = 1.0f;
     public float period = 1.0f;
+    private float posPerc = 0.0f;
+    private float delayBeforeCrush = 0.0f;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        delayBeforeCrush = rate * crushPhaseShift;
     }
 
     // Update is called once per frame
     void Update()
     {
-        float posPerc = 0.0f;
+        if(delayBeforeCrush > 0.0f)
+        {
+            delayBeforeCrush -= Time.deltaTime;
+            return;
+        }
         posPerc = (Mathf.Cos(Time.timeSinceLevelLoad/period) * 0.5f + 0.5f) * rate;
         posPerc = Mathf.Clamp01(posPerc);
         smasher.transform.position = posPerc * start.transform.position + (1.0f - posPerc) * end.transform.position;
