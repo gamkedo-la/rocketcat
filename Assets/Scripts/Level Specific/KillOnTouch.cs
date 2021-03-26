3c69afd1d8e1d08940f1ebd22a03add8edda5a32
Jeremiah Franczyk <thefranczyk@gmail.com>
2021-03-25 22:15:19 -0400

    Added script KillOnTouch to crusher prefab

---
+++ Assets/Scripts/Level Specific/KillOnTouch.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class KillOnTouch : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            Destroy(other.gameObject);
+            GameManager.instance.DelayThenLoadScene(false);
+
+        }
+    }
+}
