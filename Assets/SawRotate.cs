64914ad1b12a38741e6f857739728cb11eea6a94
Pizzaslice3 <lcpiazza626@gmail.com>
2021-03-28 14:47:09 -0500

    -sawblade script created and added to saw blade prefab!

---
+++ Assets/SawRotate.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SawRotate : MonoBehaviour
+{
+    public bool doesRotate;
+    public bool rotateCounterClockwise;
+    public float rotateSpeed = 10f;
+
+    private int directionNum = -1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (rotateCounterClockwise)
+            directionNum = 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(doesRotate)
+        transform.Rotate(Vector3.up * Time.deltaTime * directionNum * rotateSpeed);
+    }
+}
