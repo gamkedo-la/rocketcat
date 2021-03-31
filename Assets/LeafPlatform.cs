80039eaf245bc201f9da066ce984f10b28b30c2f
Jeremiah Franczyk <thefranczyk@gmail.com>
2021-03-30 22:43:43 -0400

    Added leaf platform prefab, fixed a bug with player stretching on leaf platforms prefabs

--- Assets/LeafPlatform.cs
+++ Assets/LeafPlatform.cs
@@ -2,12 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MovingPlatform : MonoBehaviour
+public class LeafPlatform : MonoBehaviour
 {
     public bool isVertical = false;
     public float distance = 7.0f;
     public float periodForHowLongLoop = 1.0f;
     public float phaseShift = 0.0f;
+
+    public float swayDistance = 8.0f;
+    public float swayTime = 1.0f;
+    public float swayShift = 0.0f;
 
     private Vector3 startPos;
     private Vector3 offsetVec;
@@ -32,30 +36,22 @@ public class MovingPlatform : MonoBehaviour
     }
 
 
-    private void OnCollisionEnter2D(Collision2D other)
-    {
-        if (other.gameObject.CompareTag("Player"))
-        {
-            other.gameObject.transform.SetParent(transform);
-        }
-    }
-
-
-    private void OnCollisionExit2D(Collision2D other)
-    {
-        if (other.gameObject.CompareTag("Player"))
-        {
-            other.gameObject.transform.SetParent(null);
-        }
-    }
-
-
     // Update is called once per frame
     void Update()
     {
         float phaseNow = 0.5f + 0.75f * Mathf.Cos(phaseShift + Time.timeSinceLevelLoad / periodForHowLongLoop); //0.75 is bigger than 0.5 so it stops on the ends
-        phaseNow = Mathf.Clamp01(phaseNow);     
-        transform.position = startPos + offsetVec * phaseNow * distance;
+        phaseNow = Mathf.Clamp01(phaseNow);
+        if (swayDistance > 0.0f)
+        {
+            float swayPhase = Mathf.Cos(swayShift + Time.timeSinceLevelLoad / swayTime);
+            float swayAng = swayPhase * 30.0f;
+            transform.rotation = Quaternion.AngleAxis(swayAng, Vector3.forward);
+            transform.position = startPos + offsetVec * phaseNow * distance + swayPhase * Vector3.right * swayDistance;
+        }
+        else
+        {
+            transform.position = startPos + offsetVec * phaseNow * distance;
+        }
     }
 
     public void OnDrawGizmos()
@@ -64,6 +60,9 @@ public class MovingPlatform : MonoBehaviour
         Gizmos.color = Color.red;
         Gizmos.DrawLine(transform.position, transform.position + offsetVec * distance);
     }
+
+
+
 
 
 }
