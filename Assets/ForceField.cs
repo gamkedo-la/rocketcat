4bbac72fc5e84e0d73c4af1c3e473ee7ebbc16a4
Jeremiah Franczyk <thefranczyk@gmail.com>
2021-03-30 21:34:18 -0400

    Force Field implemented

---
+++ Assets/ForceField.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ForceField : MonoBehaviour
+{
+    private float percOrig = 0.98f;
+    private float fieldForce = 15.0f;
+
+
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            Rigidbody2D rb = other.gameObject.GetComponent<Rigidbody2D>();
+            Vector2 newVel = (transform.up * fieldForce) * (1.0f - percOrig);
+            Vector2 oldVel = rb.velocity * percOrig;
+            rb.velocity = newVel + oldVel;
+        }
+       
+    }
+
+
+}
