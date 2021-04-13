b9bd6fcef7748d36102a41301a44e08e6850e1e0
Christer Kaitila <breakdance@mcfunkypants.com>
2021-04-13 13:55:32 -0700

    cinematic black bars added to boss battle 1 & 2

---
+++ Assets/Scripts/UI & Collectibles/imageFadeOut.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class imageFadeOut : MonoBehaviour
+{
+    public int delayFrames = 120;
+    public float fadeSpeed = 0.01f;
+    private Image img;
+    private Color rgba;
+    private int frameCounter = 0;
+    
+    void Start()
+    {
+        img = GetComponent<Image>();
+        rgba = img.color;
+    }
+
+    void Update()
+    {
+        frameCounter++;
+        if (frameCounter>delayFrames && rgba.a > 0f) {
+            rgba = img.color;
+            rgba.a -= fadeSpeed;
+            if (rgba.a<0f)rgba.a=0f;
+            img.color = rgba;
+        }
+    }
+}
