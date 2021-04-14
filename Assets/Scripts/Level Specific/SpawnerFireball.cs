5ed7c04c0b4c73c85d8b43520f4fbaf988f0b936
Jeremiah Franczyk <thefranczyk@gmail.com>
2021-04-14 00:07:57 -0400

    Added fireballSpawner WIP

---
+++ Assets/Scripts/Level Specific/SpawnerFireball.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnerFireball : MonoBehaviour
+{
+    public static SpawnerFireball instance;
+
+    public Transform newSpawn;
+    public GameObject prefabToSpawn;
+    public float minSpawnDelay = 2f;
+    public float maxSpawnDelay = 5f;
+    public float destroyTime = 10.0f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(SpawnRepeatedly());
+    }
+
+    IEnumerator SpawnRepeatedly()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(Random.Range(minSpawnDelay, maxSpawnDelay));
+            {
+                GameObject newGO = GameObject.Instantiate(prefabToSpawn);
+                if (newSpawn != null)
+                {
+                    newGO.transform.SetParent(newSpawn);
+                    newGO.transform.position = newSpawn.transform.position;
+                    Destroy(newGO, destroyTime);
+                }
+                else
+                {
+                    Debug.Log("Please set contained spawn");
+                }
+            }
+        }
+    }
+}
