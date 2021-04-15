d030e0c032c97f752e4feecdca17803569936666
Christer Kaitila <breakdance@mcfunkypants.com>
2021-04-15 16:03:22 -0700

    bugfix destroyable (don't assume eggs exist)

--- Assets/Scripts/Enemies/Destroyable.cs
+++ Assets/Scripts/Enemies/Destroyable.cs
@@ -6,33 +6,41 @@ public class Destroyable : MonoBehaviour
 {
     public bool destroyParentAlso = false;
 
+    public int HP = 1;
 
-    public void DestroySelf()
+    public void DestroySelf() // called every time an explosion touches this
     {
-        // next three lines used in level 16 for destroyed eggs
-        EggsDestroyed eggScript = GameObject.FindGameObjectWithTag("Egg").GetComponent<EggsDestroyed>();
-        if (CompareTag("Egg"))
-        {
-            eggScript.MakeWarpers();
-            Destroy(gameObject);
-            return;
-        }
-        //spawnlings used in level 4 to fix bug with spawners
-        Destroyable[] spawnlingsToDestroy = GetComponentsInChildren<Destroyable>();
-        for(int i = 0; i < spawnlingsToDestroy.Length; i++)
-        {
-            if (spawnlingsToDestroy[i] != this)
-            {
-                spawnlingsToDestroy[i].DestroySelf();
+        HP--;
+        if (HP<1) {
+            // next three lines used in level 16 for destroyed eggs
+            GameObject foundObj = GameObject.FindGameObjectWithTag("Egg");
+            if (foundObj) { // can be blank!!!
+                EggsDestroyed eggScript = foundObj.GetComponent<EggsDestroyed>();
+                if (CompareTag("Egg"))
+                {
+                    eggScript.MakeWarpers();
+                    Destroy(gameObject);
+                    return;
+                }
             }
+            //spawnlings used in level 4 to fix bug with spawners
+            Destroyable[] spawnlingsToDestroy = GetComponentsInChildren<Destroyable>();
+            for(int i = 0; i < spawnlingsToDestroy.Length; i++)
+            {
+                if (spawnlingsToDestroy[i] != this)
+                {
+                    spawnlingsToDestroy[i].DestroySelf();
+                }
+            }
+            Destroy(gameObject);
+            if (destroyParentAlso)
+            {
+                Destroy(transform.parent.gameObject);
+            }
+            
+            EnemyCountUpdate.instance.RemoveFromCounterNextFrame();
+        } else {
+            Debug.Log(gameObject.name+" HP: "+HP);
         }
-        Destroy(gameObject);
-        if (destroyParentAlso)
-        {
-            Destroy(transform.parent.gameObject);
-        }
-        
-        EnemyCountUpdate.instance.RemoveFromCounterNextFrame();
     }
-
 }
