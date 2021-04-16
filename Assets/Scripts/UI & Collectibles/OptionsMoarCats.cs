34d4e1af1b062e2ee287c17c34ba600d4fa0c007
Jeremiah Franczyk <thefranczyk@gmail.com>
2021-04-15 22:31:41 -0400

    Options menu updated- graphics quality cats now a thing...

--- Assets/Scripts/UI & Collectibles/OptionsMoarCats.cs
+++ Assets/Scripts/UI & Collectibles/OptionsMoarCats.cs
@@ -1,18 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class OptionsMoarCats : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameObject someCats;
+    public GameObject moreCats;
+    public GameObject allTheCats;
+
+
+    public void ChangeBox(TMP_Dropdown catBox)
     {
-        
+        someCats.SetActive(catBox.value >= 1);
+        moreCats.SetActive(catBox.value >= 2);
+        allTheCats.SetActive(catBox.value >= 3);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
 }
