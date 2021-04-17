2c3e8989a220f86bdfbab8f91193f358c9cf5a61
Christer Kaitila <breakdance@mcfunkypants.com>
2021-04-17 11:45:12 -0700

    boss battle healthbar ♥

--- Assets/Scripts/Enemies/Destroyable.cs
+++ Assets/Scripts/Enemies/Destroyable.cs
@@ -1,21 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Destroyable : MonoBehaviour
 {
     public bool destroyParentAlso = false;
-
     public int HP = 1;
-
     public GameObject spawnOnHit;
     public GameObject spawnOnDestroy;
+    public Text healthbarText;
     
     public void DestroySelf() // called every time an explosion touches this
     {
         HP--;
         if (HP<1) {
 
+            //need to destroy the entire healthbar parent object not just this:
+            //if (healthbarText) healthbarText.setActive(false);
+            
             if (spawnOnDestroy) Instantiate(spawnOnDestroy, transform.position, Quaternion.identity);
 
             // next three lines used in level 16 for destroyed eggs
@@ -46,6 +49,16 @@ public class Destroyable : MonoBehaviour
         } else { // not dead yet
         
             Debug.Log(gameObject.name+" HP: "+HP);
+            
+            if (healthbarText) {
+                string hearts = "BOSS ";
+                for (int n=0; n<HP; n++) {
+                    hearts = hearts + "♥";
+                }
+                healthbarText.text = hearts;
+                Debug.Log("NEW BOSS HP TEXT:"+hearts);
+            }
+            
             if (spawnOnHit) Instantiate(spawnOnHit, transform.position, Quaternion.identity);
             EnemyBossFinaleDamageFlash flasher = GetComponentInChildren<EnemyBossFinaleDamageFlash>();
             if (flasher)
